7f93b0e5ae12060dd672959857b3436d64a6ce26
Dimluar <148585902+Dimluar@users.noreply.github.com>
2023-11-29 17:39:20 +0100

    Add prepend method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,10 +1,26 @@
 # frozen_string_literal: true
 
+require_relative 'node'
+
 # Linked list data structure
 class LinkedList
-  attr_accessor :head
+  attr_reader :head, :tail
 
   def initialize
     @head = nil
+    @tail = nil
+  end
+
+  def prepend(value)
+    @head = head.nil? ? @head = Node.new(value) : Node.new(value, head)
+    set_tail
+  end
+
+  private
+
+  def set_tail
+    tmp_head = head
+    tmp_head = tmp_head.next_node until tmp_head.next_node.nil?
+    @tail = tmp_head
   end
 end
